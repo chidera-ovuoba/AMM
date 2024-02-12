da9aeaa1e9519e2021c768d74b4ac7f6893343f9
Ovuoba <ovuobachidera18@gmail.com>
2024-02-12 19:52:41 +0100

    Querying XRP Ledger completed

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const xrpl = require("xrpl");
 async function main(){
   
 const client = new xrpl.Client("wss://s.altnet.rippletest.net:51233");
-await client.connect().then(()=>console.log('Connected to the XRP Ledger'));
+await client.connect();
 
-const func_account = 
+const fund_account = await client.fundWallet();
+const test_wallet = fund_account.wallet
+// Querying the XRP Ledger for getting info about an address
+const reponse = await client.request({
+    "command":"account_info",
+    "account":test_wallet.classicAddress,
+    "ledger_index":"validated"
+});
+// listening on Ledger close event 
+console.log(reponse);
 await client.disconnect();
 }
 main()
